72cfc06313061dbe5dccfba88e555e90c8971fce
Yoshinori Yasuda <a2041yy@aiit.ac.jp>
2020-07-28 17:42:26 +0900

    絞り込み作成中

--- aiit_courses.rb
+++ aiit_courses.rb
@@ -1,6 +1,11 @@
 require 'bundler/setup'
 Bundler.require
 
+ST = 2
+SA = 3
+PM = 4
+TS = 5
+
 require 'sinatra'
 if development?
   require 'sinatra/reloader'
@@ -12,11 +17,22 @@ get '/' do
   erb :index
 end
 
-get '/erb' do
-  @message = 'コラボレイティブ開発特論'
-  erb :erb
+post '/refine' do
+  @results = RESULTS.select do |result|
+    show = true
+    if params[:sspt] && !params[:sspt].empty?
+      show = (params[:sspt].include?('ST') && !result[ST].empty?) || (params[:sspt].include?('SA') && !result[SA].empty?) || (params[:sspt].include?('PM') && !result[PM].empty?) || (params[:sspt].include?('TS') && !result[TS].empty?)
+    end
+    show
+  end
+  erb :index
 end
 
-get '/haml' do
-  haml :haml
-end
+# get '/erb' do
+#   @message = 'コラボレイティブ開発特論'
+#   erb :erb
+# end
+# 
+# get '/haml' do
+#   haml :haml
+# end
